d22d8e1d8336057fcb8393db471718bc89a9a406
Matt DiBiaso <120879643+Bourn3Dynsty@users.noreply.github.com>
2023-06-16 11:29:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import openai
 import os
 
-application = Flask(__name__)
+app = Flask(__name__)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/generate_plan', methods=['POST'])
@@ -48,7 +48,6 @@ def parse_gpt_response(response):
     # Here, we just return the generated text, but you might need to process it further depending on your needs.
     return {'plan': response.choices[0].text.strip()}
 
-...
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
 
