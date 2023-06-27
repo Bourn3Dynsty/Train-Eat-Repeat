61ec68e93b10abe2a113c01c695518dce86e13af
Matt DiBiaso <120879643+Bourn3Dynsty@users.noreply.github.com>
2023-06-27 15:16:52 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,32 +5,35 @@ import boto3
 from flask_cors import CORS
 from threading import Thread
 import random
+import PyPDF2
 
-application = Flask(__name__)
-CORS(application)  # Enable CORS on your Flask app
-
-
+app = Flask(__name__)
+CORS(app)  # Enable CORS on your Flask app
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
+file_path = r"C:\Users\mdibiaso\Repos\Train-Eat-Repeat\6daysplit.pdf"
 
 
 
-@application.route('/generate_plan', methods=['POST'])
+
+@app.route('/generate_plan', methods=['POST'])
 def generate_plan():
     fitness_data = request.json
 
     print(fitness_data)  # Print the received data
+    keys = list(fitness_data.keys())
+    print(keys)
 
     if fitness_data is None:
+        print(fitness_data)
         return jsonify({'message': 'No data provided'}), 400  # Bad Request
 
-    required_keys = ["height", "weight", "age", "sex", "bmi", "fitness_level", "goal"]
+    required_keys = ["height_feet", "height_inches", "weight", "age", "sex", "bmi", "fitness_level", "goal", "email"]
+
     print(list(fitness_data.keys()))  # Print the keys in the received data
     for key in required_keys:
         if key not in fitness_data:
             return jsonify({'message': f'Missing key in data: {key}'}), 400
-
-
 
     goal = fitness_data["goal"]
     if goal == "fat_loss":
@@ -58,9 +61,23 @@ def generate_plan():
     workout_plan = parse_workout_response(workout_response)
     meal_plan = parse_meal_response(meal_response)
 
-    send_email(workout_plan, meal_plan)
+    receiver_email = fitness_data.get('email')  # Get the email from the form data
+    send_email(receiver_email, workout_plan, meal_plan)  # Pass the email to the send_email function
 
     return jsonify({'message': 'Cooking something good!. The plans will be sent via email'})
+
+def read_pdf(file_path):
+    pdf_file_obj = open(file_path, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+    num_pages = pdf_reader.numPages
+
+    content = ""
+    for i in range(num_pages):
+        page_obj = pdf_reader.getPage(i)
+        content += page_obj.extractText()
+
+    pdf_file_obj.close()
+    return content
 
 
 
@@ -68,54 +85,67 @@ def format_workout_prompt(fitness_data):
     # Determine the number of workout days based on fitness level
     fitness_level = fitness_data["fitness_level"]
     workout_type = fitness_data.get("workout_type", "general")  # Fetch workout type if present, otherwise default to "general"
+
     if fitness_level == "beginner":
         workout_days = 3
-        exercise_rep_ranges = "3 sets of 8-10 reps"
     elif fitness_level == "intermediate":
         workout_days = 5
-        exercise_rep_ranges = "3 sets of 10-12 reps"
     elif fitness_level == "advanced":
         workout_days = 6
-        exercise_rep_ranges = "3 sets of 12-15 reps"
     else:
         workout_days = 3  # Default to 3 workout days for unknown fitness levels
-        exercise_rep_ranges = "3 sets of 8-10 reps"  # Default rep range
-
-    # Retrieve the height value from the form field
-    height = fitness_data["height"]
 
     # Create a prompt for generating the workout plan
     prompt = (
-        "As a NASM-certified personal trainer with 20 years of experience, I am helping a user create a workout plan. "
-        "The user's details are as follows:\n"
-        "Height: {}\n"
+        "Based on this information, create a personalized {}-day workout plan with 5 exercises per day:\n\n"
+        "Height (feet): {}\n"
+        "Height (inches): {}\n"
         "Weight: {}\n"
         "Age: {}\n"
         "Sex: {}\n"
         "BMI: {}\n"
         "Fitness Level: {}\n"
         "Workout Type: {}\n"
-        "Based on this information, create a personalized {}-day workout plan with 5-8 exercises per day depending on fitness level:\n\n"
-    ).format(
-        height,
-        fitness_data["weight"],
-        fitness_data["age"],
-        fitness_data["sex"],
-        fitness_data["bmi"],
-        fitness_data["fitness_level"],
-        workout_type,
-        workout_days,
-    )
+).format(
+    workout_days,
+    fitness_data["height_feet"],
+    fitness_data["height_inches"],
+    fitness_data["weight"],
+    fitness_data["age"],
+    fitness_data["sex"],
+    fitness_data["bmi"],
+    fitness_data["fitness_level"],
+    workout_type
+)
 
     # Add exercise prompts for each day
     for day in range(1, workout_days + 1):
-        prompt += f"Day {day}:\n"
-        for exercise_num in range(1, 6):
+        if workout_type == "fat_loss":
+            prompt += f"Day {day} (Steady-State Cardio and Light Weights):\n"
+        elif workout_type == "body_building":
+            if day % 3 == 1:
+                prompt += f"Day {day} (Push):\n"
+            elif day % 3 == 2:
+                prompt += f"Day {day} (Pull):\n"
+            else:
+                prompt += f"Day {day} (Legs):\n"
+        else:  # For general or unknown workout type
+            prompt += f"Day {day}:\n"
+
+        for exercise_num in range(1, 8):
+            if fitness_level == "beginner":
+                exercise_rep_ranges = "3 sets of 8-10 reps"
+            elif fitness_level == "intermediate":
+                exercise_rep_ranges = "3 sets of 10-12 reps"
+            elif fitness_level == "advanced":
+                exercise_rep_ranges = "4 sets of 12-15 reps"
+            else:
+                exercise_rep_ranges = "3 sets of 8-10 reps"  # Default rep range
+
             prompt += f"Exercise {exercise_num}: [Exercise Name] - {exercise_rep_ranges}\n"
         prompt += "\n"
 
     return prompt
-
 
 
 
@@ -141,7 +171,7 @@ def format_fat_loss_workout_prompt(fitness_data):
 
     # Create a prompt for generating the workout plan
     prompt = (
-        "As a NASM-certified personal trainer with 20 years of experience, I am helping a user create a workout plan for fat loss that has cardio and a little bit of light weights"
+        "As a NASM-certified personal trainer with 20 years of experience, I am helping a user create a workout plan for fat loss that has steady state cardio and a little bit of light weights"
         "The user's details are as follows:\n"
         "Height: {}\n"
         "Weight: {}\n"
@@ -150,7 +180,7 @@ def format_fat_loss_workout_prompt(fitness_data):
         "BMI: {}\n"
         "Fitness Level: {}\n"
         "Workout Type: {}\n"
-        "Based on this information and the goal of fat loss, create a personalized {}-day workout plan with 5-8 exercises per day cardio and light weight excercises depending on fitness level:\n\n"
+        "Based on this information and the goal of fat loss, create a personalized {}-day workout plan with 5-8 exercises per day steady state cardio and light weight excercises depending on fitness level:\n\n"
     ).format(
         height,
         fitness_data["weight"],
@@ -173,8 +203,8 @@ def format_fat_loss_workout_prompt(fitness_data):
 
 
 
-def format_bodybuilding_workout_prompt(fitness_data):
-    # Determine the number of workout days based on fitness level
+def format_bodybuilding_workout_prompt(fitness_data, file_path):
+    pdf_content = read_pdf(file_path)
     fitness_level = fitness_data["fitness_level"]
     workout_type = fitness_data.get("workout_type", "general")  # Fetch workout type if present, otherwise default to "general"
     if fitness_level == "beginner":
@@ -201,7 +231,8 @@ def format_bodybuilding_workout_prompt(fitness_data):
         "BMI: {}\n"
         "Fitness Level: {}\n"
         "Workout Type: {}\n"
-        "Based on this information and the goal of bodybuilding, create a personalized {}-day push-pull-legs (PPL) workout plan with 5-9 exercises per day depending on fitness level that focuses on making push-pull-legs splits:\n\n"
+        "Workout Plans from PDF: {}\n"
+        "Based on this information and the goal of bodybuilding, create a personalized {}-day push-pull-legs (PPL) workout plan with 5-9 exercises per day depending on fitness level that focuses on making upper body push and pull and 1-2 dedicated legs splits do not repeat expercises:\n\n"
     ).format(
         fitness_data["height"],
         fitness_data["weight"],
@@ -211,6 +242,7 @@ def format_bodybuilding_workout_prompt(fitness_data):
         fitness_data["fitness_level"],
         workout_type,
         workout_days,
+        pdf_content
     )
 
     # Add exercise prompts for each day
@@ -301,10 +333,9 @@ def parse_meal_response(response):
     return formatted_plan
 
 
-def send_email(workout_plan, meal_plan):
+def send_email(receiver_email, workout_plan, meal_plan):
     # Set up the email
     sender_email = os.getenv("SENDER_EMAIL")
-    receiver_email = os.getenv("RECEIVER_EMAIL")
     subject = "Personalized Workout and Meal Plans"
 
     # Generate a random motivational quote
@@ -341,22 +372,21 @@ def send_email(workout_plan, meal_plan):
     Best,
     [Your Name]
     """
-    
-    message = {
-        'Subject': {'Data': subject},
-        'Body': {'Text': {'Data': message_body}}
-    }
-    
-    client = boto3.client('ses', region_name='us-east-1')  # Replace 'us-west-2' with your desired region
+
+    # Create an AWS SES client
+    client = boto3.client("ses", region_name="us-east-1")  # Replace 'us-east-1' with your desired region
+
+    # Send the email
     response = client.send_email(
         Source=sender_email,
-        Destination={'ToAddresses': [receiver_email]},
-        Message=message
+        Destination={"ToAddresses": [receiver_email]},
+        Message={
+            "Subject": {"Data": subject},
+            "Body": {"Text": {"Data": message_body}},
+        },
     )
 
     return response
 
-
-
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
